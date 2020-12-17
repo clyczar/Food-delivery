153c7e57cafb9fe733ac5cdff8321b6d34181605
clyczar <41852445+clyczar@users.noreply.github.com>
2020-12-17 13:54:21 +1030

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { CommonService } from './services/common.service';
+
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientJsonpModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PcontentComponent } from './components/pcontent/pcontent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PcontentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientJsonpModule
+  ],
+  providers: [CommonService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
